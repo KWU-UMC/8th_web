de7a40fa11adba7e965fd3359fa8e8dd91ed9d99
gpdnjs8 <jade3735@naver.com>
2025-04-01 21:14:21 +0900

    feat: 영화 상세 페이지 라우팅

--- week03/mission3/src/App.tsx
+++ week03/mission3/src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MoviesPage from './pages/MoviesPage';
 import ErrorPage from './pages/ErrorPage';
+import DetailPage from './pages/DetailPage';
 import RootLayout from './layout/root-layout';
 
 const router = createBrowserRouter([
@@ -11,6 +12,9 @@ const router = createBrowserRouter([
     children: [
       { index: true, element: <MoviesPage /> },  
       { path: 'movies/:category', element: <MoviesPage /> },  
+      {
+        path: 'movies/detail/:movieId', element: <DetailPage />
+      }
     ],
     errorElement: <ErrorPage />,
   },
--- week03/mission3/src/layout/root-layout.tsx
+++ week03/mission3/src/layout/root-layout.tsx
@@ -1,13 +1,14 @@
-/* root-layout */
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Navbar from '../components/Navbar';
 
-
 const RootLayout = () => {
+  const location = useLocation();
+  const isDetailPage = location.pathname.startsWith('/movies/detail/') && location.pathname.split('/').length === 4;
+
   return (
     <>
-    <Navbar/>
-    <Outlet/>
+      {!isDetailPage && <Navbar />} 
+      <Outlet />
     </>
   );
 };
