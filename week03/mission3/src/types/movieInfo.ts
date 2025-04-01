ecdbf74c5e70e6d51ddc87538aa9996ec9726df9
gpdnjs8 <jade3735@naver.com>
2025-04-01 21:16:42 +0900

    feat: 영화 상세 페이지 구현, 타입 정의

--- week03/mission3/src/types/movieInfo.ts
+++ week03/mission3/src/types/movieInfo.ts
@@ -4,3 +4,87 @@ export interface TmovieInfo {
     title: string;
     overview: string;
 }
+
+export interface Genre {
+    id: number;
+    name: string;
+}
+
+export interface ProductionCompany {
+    id: number;
+    logo_path: string | null;
+    name: string;
+    origin_country: string;
+}
+
+export interface ProductionCountry {
+    iso_3166_1: string;
+    name: string;
+}
+
+export interface SpokenLanguage {
+    english_name: string;
+    iso_639_1: string;
+    name: string;
+}
+
+export interface TMovieDetails {
+    adult: boolean;
+    backdrop_path: string | null;
+    belongs_to_collection: string | null;
+    budget: number;
+    genres: Genre[];
+    id: number;
+    imdb_id: string | null;
+    original_language: string;
+    original_title: string;
+    overview: string;
+    popularity: number;
+    poster_path: string | null;
+    production_companies: ProductionCompany[];
+    production_countries: ProductionCountry[];
+    release_date: string;
+    revenue: number;
+    runtime: number;
+    spoken_languages: SpokenLanguage[];
+    status: string;
+    tagline: string | null;
+    title: string;
+    video: boolean;
+    vote_average: number;
+    vote_count: number;
+}
+
+export interface TCastMember {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+    cast_id: number;
+    character: string;
+    credit_id: string;
+    order: number;
+}
+
+export interface TCrewMember {
+    adult: boolean;
+    gender: number;
+    id: number;
+    known_for_department: string;
+    name: string;
+    original_name: string;
+    popularity: number;
+    profile_path: string;
+    credit_id: string;
+    department: string;
+    job: string;
+}
+
+export interface TCredits {
+    cast: TCastMember[];  
+    crew: TCrewMember[]; 
+}
