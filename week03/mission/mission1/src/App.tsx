1355e25f33407916779673abbdc1493e3199fd8d
potatodev-777 <potatodev777@gmail.com>
2025-04-03 11:20:31 +0900

    data fetching

--- week03/mission/mission1/src/App.tsx
+++ week03/mission/mission1/src/App.tsx
@@ -1,7 +1,49 @@
-import "./App.css";
+import { useEffect, useState } from "react";
+import { Movie, MovieResponse } from "./types/movie_type";
 
 function App() {
-  return <></>;
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const url =
+        "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1";
+      const options = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+          Authorization: `Bearer ${import.meta.env.VITE_MOVIE_API}`,
+        },
+      };
+
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error(`API request error: ${response.status}`);
+        }
+        const data: MovieResponse = await response.json();
+        setMovies(data.results);
+        setIsLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  if (isLoading) return null;
+
+  return (
+    <div className="w-full h-screen">
+      <ul>
+        {movies?.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
