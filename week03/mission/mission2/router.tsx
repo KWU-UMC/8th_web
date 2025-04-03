5a6a394720e41042dd85e4f008120eb5ee4813fd
potatodev-777 <potatodev777@gmail.com>
2025-04-03 17:48:49 +0900

    페이지 라우팅 기본 세팅

--- week03/mission/mission2/router.tsx
+++ week03/mission/mission2/router.tsx
@@ -1,1 +1,34 @@
-export default function Router() {}
+import { createBrowserRouter } from "react-router-dom";
+import App from "./src/App";
+import React from "react";
+import Popular from "./src/pages/popular";
+import Upcoming from "./src/pages/upcoming";
+import Toprated from "./src/pages/toprated";
+import Nowplaying from "./src/pages/nowplaying";
+import Errorpage from "./src/pages/errorpage";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Errorpage />,
+    children: [
+      {
+        path: "/popular",
+        element: <Popular />,
+      },
+      {
+        path: "/upcoming",
+        element: <Upcoming />,
+      },
+      {
+        path: "/toprated",
+        element: <Toprated />,
+      },
+      {
+        path: "/nowplaying",
+        element: <Nowplaying />,
+      },
+    ],
+  },
+]);
