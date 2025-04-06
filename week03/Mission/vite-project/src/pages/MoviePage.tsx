d1343e7bb2a35125bdf0ba840152101e90896206
seoyeon0103 <angel6462@naver.com>
2025-04-06 20:06:42 +0900

    ,

--- week03/Mission/vite-project/src/pages/MoviePage.tsx
+++ week03/Mission/vite-project/src/pages/MoviePage.tsx
@@ -9,7 +9,7 @@ export default function MoviePage(){
     useEffect((): void => {
         const fetchMovies = async () : Promise<void>=>{
             const{ data } = await axios.get<MovieResponse>(
-                `https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`,
+                `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`,
                 {
                     headers : {
                         Authorization: `Bearer ${import.meta.env.VITE_TMDB_KEY}`,
@@ -23,8 +23,9 @@ export default function MoviePage(){
     }, []);
 
     return(
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 
-        lg: grid-cols-5 xl: grid-cols-6'>
+        //반응형
+        <div className='p-10 grid gap-4 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 
+        lg:grid-cols-5 xl:grid-cols-6'>
             {movies?.map( movie => (
                 <MovieCard key={movie.id} moive={movie}/>
             ))}
