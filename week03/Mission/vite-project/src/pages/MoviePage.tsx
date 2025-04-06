b5bdb2f42a78f6a9dc1100780970a4211e5028b0
seoyeon0103 <angel6462@naver.com>
2025-04-06 20:28:21 +0900

    Feat: [Mission 2] 에러 처리 관련한 UI 꾸미기

--- week03/Mission/vite-project/src/pages/MoviePage.tsx
+++ week03/Mission/vite-project/src/pages/MoviePage.tsx
@@ -2,15 +2,19 @@ import axios from "axios";
 import { useEffect,useState } from "react";
 import { Movie, MovieResponse} from "../types/movie";
 import MovieCard from "../components/MovieCard";
+import { LoadingSpinner } from "../components/LoadingSpinner";
 
 export default function MoviePage(){
     const [movies, setMovies] = useState<Movie[]>([]);
     
     const [isPending, setIsPending] = useState(false);
     const [isError, setIsError] = useState(false);
-    
+
     useEffect((): void => {
-        const fetchMovies = async () : Promise<void>=>{
+    const fetchMovies = async () : Promise<void>=>{
+        setIsPending(true);
+
+        try{
             const{ data } = await axios.get<MovieResponse>(
                 `https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1`,
                 {
@@ -20,10 +24,24 @@ export default function MoviePage(){
                 }
             );
 
-            setMovies(data.results);
-        };
-        fetchMovies();
+            setMovies(data.results);        
+        } catch {
+            setIsError(true);
+        }finally{
+            setIsPending(false);
+        }
+    };
+    
+    fetchMovies();  
     }, []);
+
+    if(isError){
+        return (
+        <div>
+            <span className="text-red-500 text-2xl"> 에러가 발생했습니다. </span>
+        </div>
+        )
+    }
 
     return(
         //반응형
