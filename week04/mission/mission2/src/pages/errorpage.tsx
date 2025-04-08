d831ba2817faa45c0fd848c5b8ec0b51e3561b1c
potatodev-777 <potatodev777@gmail.com>
2025-04-08 14:33:34 +0900

    mission2 setting-f

--- week04/mission/mission2/src/pages/errorpage.tsx
+++
@@ -1,7 +0,0 @@
-export default function Errorpage() {
-  return (
-    <div className="w-full h-screen px-30 mb-20 box-border flex justify-center items-center">
-      <h1>에러 발생 ㅜㅜ</h1>
-    </div>
-  );
-}
--- week04/mission/mission2/src/pages/home.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div className="w-full h-screen px-30 mb-20 box-border flex justify-center items-center">
-      <h2>Welcome</h2>
-    </div>
-  );
-}
