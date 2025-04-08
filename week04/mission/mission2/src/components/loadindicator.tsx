2c8f09ee4210d1685e86aa08a2b15978e1c0f3e6
potatodev-777 <potatodev777@gmail.com>
2025-04-08 13:52:21 +0900

    mission2 setting

---
+++ week04/mission/mission2/src/components/loadindicator.tsx
@@ -0,0 +1,7 @@
+export default function Loadindicator() {
+  return (
+    <div className="w-full h-screen px-30 mb-20 box-border flex justify-center items-center">
+      <div className="w-20 h-20 border-8 border-gray-200 border-t-blue-500 rounded-full animate-spin"></div>
+    </div>
+  );
+}
---
+++ week04/mission/mission2/src/pages/home.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full h-screen px-30 mb-20 box-border flex justify-center items-center">
+      <h2>Welcome</h2>
+    </div>
+  );
+}
