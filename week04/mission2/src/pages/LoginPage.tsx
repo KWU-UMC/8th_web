866a8d1fb79e6f24ebbe03e2263af18df9e52e53
gpdnjs8 <jade3735@naver.com>
2025-04-08 22:02:09 +0900

    feat: 로그인 성공 시 메인 페이지 이동 및 로그인 성공/실패 콘솔 로그 추가

--- week04/mission2/src/pages/LoginPage.tsx
+++ week04/mission2/src/pages/LoginPage.tsx
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Arrow from '../images/Arrow.svg';
 import GoogleLoginButton from "../components/GoogleLoginBtn";
 import InputField from "../components/InputField";
 import { validateLogin } from "../utils/validate";
 import { useForm } from "../hooks/useForm";
+import axios from "axios";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
+
     const login = useForm({
         initialValue: { email: "", password: "" },
         validate: validateLogin,
@@ -14,12 +17,30 @@ const LoginPage = () => {
     const isFormValid = !login.errors.email && !login.errors.password && login.values.email && login.values.password;
 
     // 확인용
-    const handlePressLogin = () => {
-        console.log("로그인:", login.values.email, login.values.password);
+    const handlePressLogin = async () => {
+        try {
+          const response = await axios.post("http://localhost:8000/v1/auth/signin", {
+            email: login.values.email,
+            password: login.values.password,
+          });
+      
+          if (response.status === 200 || response.status === 201) {
+            console.log("로그인 성공", response.data);
+            navigate("/");      // 로그인 성공 시 메인 페이지로 이동
+          } else {
+            console.warn("예상치 못한 응답", response.status);
+          }
+        } catch (error: any) {
+          if (error.response) {
+            console.error("로그인 실패", error.response.data.message || error.message);
+          } else {
+            console.error("서버 연결 실패", error.message);
+          }
+        }
     };
 
     return (
-        <div className="min-h-screen bg-black text-white flex justify-center items-start pt-20">
+        <div className="h-[700px] bg-black text-white flex justify-center items-start pt-20">
             <div className="w-full max-w-sm p-4">
                 <div className="relative mb-6">
                     <Link to="/" className="absolute left-0 top-1/2 -translate-y-[45%]">
