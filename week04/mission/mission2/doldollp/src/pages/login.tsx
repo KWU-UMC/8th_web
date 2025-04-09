dccb89cbb9b3c0911d56dc51dce658e06a3fe491
potatodev-777 <potatodev777@gmail.com>
2025-04-09 11:03:12 +0900

    finish login.tsx-login

--- week04/mission/mission2/doldollp/src/pages/login.tsx
+++ week04/mission/mission2/doldollp/src/pages/login.tsx
@@ -1,8 +1,40 @@
+import { useState } from "react";
 import { FcGoogle } from "react-icons/fc";
 import { useNavigate } from "react-router-dom";
 
 export default function Login() {
+  const [email, setEmail] = useState<string>("");
+  const [isValidEmail, setIsValidEmail] = useState<boolean>(true);
+  const [password, setPassword] = useState<string>("");
+  const [isValidPassword, setIsValidPassword] = useState<boolean>(true);
   const navigate = useNavigate();
+
+  const checkValidEmail = (value: string) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return emailRegex.test(value);
+  };
+  const checkValidPassword = (value: string) => {
+    return value.length >= 8 ? true : false;
+  };
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") {
+      setIsValidEmail(true);
+    } else {
+      setIsValidEmail(checkValidEmail(e.target.value));
+    }
+
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") {
+      setIsValidPassword(true);
+    } else {
+      setIsValidPassword(checkValidPassword(e.target.value));
+    }
+
+    setPassword(e.target.value);
+  };
 
   return (
     <div className="flex flex-col gap-6 w-100">
@@ -24,14 +56,36 @@ export default function Login() {
       <div>
         <form className="flex flex-col gap-4 w-full">
           <input
+            onChange={handleEmailChange}
             className="w-full p-2 bg-black border border-white rounded-xl"
             placeholder="이메일을 입력해주세요!"
           />
+          {isValidEmail || (
+            <span className="text-md text-red-600">
+              올바른 이메일 형식을 입력해주세요.
+            </span>
+          )}
           <input
+            type="password"
+            onChange={handlePasswordChange}
             className="w-full p-2 bg-black border border-white rounded-xl"
             placeholder="비밀번호를 입력해주세요!"
           />
-          <button className="w-full bg-black rounded-xl" type="submit">
+          {isValidPassword || (
+            <span className="text-md text-red-600">
+              비밀번호는 8자 이상이어야 합니다.
+            </span>
+          )}
+          <button
+            className={`w-full bg-black rounded-xl ${
+              isValidEmail &&
+              isValidPassword &&
+              email &&
+              password &&
+              "bg-pink-300"
+            }`}
+            type="submit"
+          >
             로그인
           </button>
         </form>
