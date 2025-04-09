3a0c50e1980bce844f5d9fedba812486ba45d49a
potatodev-777 <potatodev777@gmail.com>
2025-04-09 10:16:46 +0900

    create login.tsx

--- week04/mission/mission2/doldollp/src/pages/login.tsx
+++ week04/mission/mission2/doldollp/src/pages/login.tsx
@@ -1,3 +1,22 @@
 export default function Login() {
-  return <h1>Login</h1>;
+  return (
+    <div>
+      <div>
+        <button></button>
+        <h2>로그인</h2>
+      </div>
+      <div>
+        <hr className="solid" />
+        <span>OR</span>
+        <hr className="solid" />
+      </div>
+      <div>
+        <form>
+          <input placeholder="이메일을 입력해주세요!" />
+          <input placeholder="비밀번호를 입력해주세요!" />
+          <button type="submit">로그인</button>
+        </form>
+      </div>
+    </div>
+  );
 }
