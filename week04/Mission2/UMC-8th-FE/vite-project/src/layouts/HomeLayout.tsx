057d8f611d3343b3f06f6eca2fa89afb35e44429
seoyeon0103 <angel6462@naver.com>
2025-04-13 23:56:34 +0900

    Feat: [Mission2] 로그인 nav 꾸미기

--- week04/Mission2/UMC-8th-FE/vite-project/src/layouts/HomeLayout.tsx
+++ week04/Mission2/UMC-8th-FE/vite-project/src/layouts/HomeLayout.tsx
@@ -2,12 +2,21 @@ import { Outlet } from "react-router-dom";
 
 const HomeLayout = () =>{
     return(
-        <div className="h-dvh flex flex-col">
-            <nav> 네비게이션 바 입니다. </nav>
+        <div className="h-dvh flex flex-col bg-black">
+            <nav className="bg-[#121210] text-pink-500 font-bold p-5 text-lg
+            flex justify-between items-right"> 
+                <div> 돌려돌려LP판 </div>
+                <div className="flex space-x-3">
+                    <button className="bg-black text-white py-1 px-3 rounded-md
+                    hover:bg-pink-600 transition-colors text-sm"> 로그인 </button>
+                    <button  className="bg-black text-white py-1 px-3 rounded-md
+                    hover:bg-pink-600 transition-colors text-sm"> 회원가입 </button>
+                </div>
+            </nav>
             <main className="flex-1"> 
                 <Outlet/> 
             </main>
-            <footer> 푸터 </footer>
+            {/* <footer> 푸터 </footer> */}
         </div>
     );
 };
