611d1cf80055e25e0336059f91970a8f9641cbda
seoyeon0103 <angel6462@naver.com>
2025-04-14 03:55:54 +0900

    Feat: [Mission3] 토큰 저장 및 불러오기

--- week04/Mission3/UMC-8th-FE/vite-project/src/apis/axios.ts
+++ week04/Mission3/UMC-8th-FE/vite-project/src/apis/axios.ts
@@ -2,4 +2,7 @@ import axios from "axios"
 
 export const axiostInstance = axios.create({
     baseURL: import.meta.env.VITE_SERVER_API_URL,
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+    },
 });
