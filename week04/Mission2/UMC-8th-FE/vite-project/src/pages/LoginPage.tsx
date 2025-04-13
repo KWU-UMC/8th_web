de3949f2fc19b076729fa31260798e9eafa6f9dd
seoyeon0103 <angel6462@naver.com>
2025-04-14 01:34:13 +0900

    Feat: [Mission2] LoginPage 구현 완료

--- week04/Mission2/UMC-8th-FE/vite-project/src/pages/LoginPage.tsx
+++ week04/Mission2/UMC-8th-FE/vite-project/src/pages/LoginPage.tsx
@@ -1,7 +1,9 @@
+import { useNavigate } from "react-router-dom";
 import useForm from "../hooks/useForm";
 import { UserSiginInformation, validateSignin } from "../utils/validate";
 
 const LoginPage = () => {
+    const navigate = useNavigate();
     const {values, errors, touched, getInputProps} = useForm<UserSiginInformation>({
         initialValue: {
             email: "",
@@ -16,10 +18,41 @@ const LoginPage = () => {
     const isDisabled =
     Object.values(errors || {}).some((error) => error.length > 0) ||
     Object.values(values).some((value) => value === "");
+    
+    //이전 페이지로 돌아가기 함수
+    const handleGoBack = () => {
+        navigate(-1);
+    }
 
     return(
         <div className="flex flex-col items-center justify-center h-full gap-4
         bg-black text-white">
+            <div className="flex justify-center items-center w-full px-4 py-4">
+                <button
+                    onClick={handleGoBack}
+                    className="text-white hover:text-pink-500 transition-colors 
+                    text-3xl w-[205px]
+                    ">
+                    {`<`}
+                </button>
+                <span className="text-white text-2xl w-[270px]"> 로그인 </span>
+            </div>
+            <div className="flex flex-col gap-3 mb-2">
+                <button
+                className={`flex items-center justify-center border border-[#ccc] 
+                    w-[300px] p-[10px] rounded-sm hover:border-pink-500 transition-colors`} >
+                    <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSfIC05_FZCiPUcZ6Xnlvfvpw3nZVBaIGbALQ&s"
+                        className="flex flex-between w-6 h-6" />    
+                        <span className="w-[240px]"> 구글 로그인 </span>
+                </button>
+            </div>
+
+            <div className="flex items-center  w-[300px] mb-2">
+                <div className="flex-grow border-t border-gray-400"></div>
+                <span className="mx-4 text-sm"> OR </span>
+                <div className="flex-grow border-t border-gray-400"></div>
+            </div>
             <div className="flex flex-col gap-3">
                 <input 
                 {...getInputProps("email")}
