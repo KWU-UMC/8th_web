c2e36954b678e18ab028962ec5a41aa9f5847e9b
potatodev-777 <potatodev777@gmail.com>
2025-04-21 19:37:00 +0900

    sidebar 열고 닫힘 메서드

--- week06/mission/mission1/src/App.tsx
+++ week06/mission/mission1/src/App.tsx
@@ -4,18 +4,37 @@ import { Outlet } from "react-router-dom";
 import Header from "./layouts/header";
 import Sidebar from "./layouts/sidebar";
 import { useSidebar } from "./contexts/sidebar";
+import { useRef } from "react";
 
 const queryClient = new QueryClient();
 
 function App() {
-  const { isOpen } = useSidebar();
+  const { isOpen, setIsOpen } = useSidebar();
+  const sidebarRef = useRef<HTMLDivElement>(null);
+
+  const handleCloseSidebar = (e: React.MouseEvent) => {
+    if (sidebarRef.current && !sidebarRef.current.contains(e.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth <= 1000) {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true);
+    }
+  });
 
   return (
     <QueryClientProvider client={queryClient}>
       <Header />
       <div className="mt-15 flex">
-        <Sidebar />
-        <div className={`w-full bg-red-300 ${isOpen ? "ml-50" : ""}`}>
+        <Sidebar ref={sidebarRef} />
+        <div
+          onClick={handleCloseSidebar}
+          className={`w-full bg-red-300 ${isOpen ? "ml-50" : ""}`}
+        >
           <Outlet />
         </div>
       </div>
--- week06/mission/mission1/src/layouts/sidebar.tsx
+++ week06/mission/mission1/src/layouts/sidebar.tsx
@@ -1,15 +1,21 @@
+import { forwardRef } from "react";
 import { useSidebar } from "../contexts/sidebar";
 import { IoSearchSharp } from "react-icons/io5";
 import { IoPerson } from "react-icons/io5";
 
-export default function Sidebar() {
+interface SidebarI extends React.HTMLProps<HTMLDivElement> {}
+
+const Sidebar = forwardRef<HTMLDivElement, SidebarI>((props, ref) => {
   const { isOpen } = useSidebar();
+  const { children, ...divProps } = props;
 
   return (
     <div
+      ref={ref}
       className={`w-50 p-4 fixed left-0 h-screen bg-blue-100 transition-transform duration-300 ease-in-out ${
         isOpen ? "translate-x-0" : "-translate-x-full"
       }`}
+      {...divProps}
     >
       <div className="flex gap-4 justify-start items-center">
         <IoSearchSharp />
@@ -22,6 +28,9 @@ export default function Sidebar() {
       <div>
         <span>탈퇴하기</span>
       </div>
+      {children}
     </div>
   );
-}
+});
+
+export default Sidebar;
