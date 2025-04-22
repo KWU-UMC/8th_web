eacbaae889aa65033d1daa3d8de05bc3b5fff23e
potatodev-777 <potatodev777@gmail.com>
2025-04-22 22:04:21 +0900

    mission2 세팅

---
+++ week06/mission/mission2/src/apis/lpapi.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+import { LPResponse, LPSResponse } from "../types/lptype";
+
+export const lps = async ({ cursor = 0, limit = 20, order = "asc" }) => {
+  const url = `${
+    import.meta.env.VITE_BASE_URL
+  }/lps?cursor=${cursor}&limit=${limit}&order=${order}`;
+
+  try {
+    const { data } = await axios.get<LPSResponse>(url);
+    return data.data.data;
+  } catch (error) {
+    console.error("lps request failed: ", error);
+  }
+};
+
+export const lp = async ({ id }: { id: string }) => {
+  const url = `${import.meta.env.VITE_BASE_URL}/lps/${id}`;
+
+  try {
+    const response = await axios.get<LPResponse>(url);
+    return response.data;
+  } catch (error) {
+    console.error("lp request failed: ", error);
+  }
+};
