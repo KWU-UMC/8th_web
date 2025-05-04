d03825a350e5d0c3a532f8ff395d1d7cd77ae622
simonnchaser <jico9954@gmail.com>
2025-05-04 17:39:08 +0900

    Protected Route를 활용하여 페이지 안전하게 보호

---
+++ week05/mission01/src/apis/axios.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { LOCAL_STORAGE_KEY } from "../constants/key";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+export const axiosInstance = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_API_URL,
+});
+
+axiosInstance.interceptors.request.use((config) => {
+  const { getItem } = useLocalStorage(LOCAL_STORAGE_KEY.accessToken);
+
+  const token = getItem(); // ✅ 그냥 localStorage 사용
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
