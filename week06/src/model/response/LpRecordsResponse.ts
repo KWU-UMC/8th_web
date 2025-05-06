097f4cad3bc880a8bc8a9ae19f90e9fbd5e26a41
onebone <jyc00410@gmail.com>
2025-05-07 03:12:22 +0900

    week06: add infinite query

--- week06/src/model/response/LpRecordsResponse.ts
+++ week06/src/model/response/LpRecordsResponse.ts
@@ -2,7 +2,9 @@ import {LpRecord} from "../LpRecord.ts";
 import {Response} from "./Response.ts";
 
 export type LpRecordData = {
-    data: LpRecord[]
+    data: LpRecord[],
+    nextCursor: boolean,
+    hasNext: boolean
 }
 
 export type LpRecordsResponse = Response<LpRecordData>
--- week06/src/model/response/Response.ts
+++ week06/src/model/response/Response.ts
@@ -2,7 +2,5 @@ export type Response<T> = {
     status: boolean,
     statusCode: number,
     message: string,
-    data: T,
-    hasNext: boolean,
-    nextCursor: number
+    data: T
 };
