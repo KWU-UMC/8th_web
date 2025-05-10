e1d89de83d9764bde935495b97e5ff04f17d03b6
seoyeon0103 <angel6462@naver.com>
2025-05-11 05:30:13 +0900

    Feat: lp.ts 구현

--- week06/Mission1/UMC-8th-FE/vite-project/src/apis/lp.ts
+++ week06/Mission1/UMC-8th-FE/vite-project/src/apis/lp.ts
@@ -1,3 +1,12 @@
-export const getLPList = async() => {
-    
+import { PaginationDto } from "../types/common";
+import { ResponseLPListDto } from "../types/lp";
+import { axiostInstance } from "./axios";
+
+export const getLPList = async(paginationDto: PaginationDto):Promise<ResponseLPListDto> => {
+    const{data} = await axiostInstance.get('v1/lps',{
+        params:paginationDto,
+
+    })
+
+    return data;
 };
