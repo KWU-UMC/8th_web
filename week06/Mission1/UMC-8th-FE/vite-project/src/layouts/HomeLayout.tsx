af661422e301a120431720103d2ba94f2ec454ed
seoyeon0103 <angel6462@naver.com>
2025-05-11 04:59:11 +0900

    Feat: Footer 구현 완료

--- week06/Mission1/UMC-8th-FE/vite-project/src/layouts/HomeLayout.tsx
+++ week06/Mission1/UMC-8th-FE/vite-project/src/layouts/HomeLayout.tsx
@@ -1,5 +1,6 @@
 import {Outlet } from "react-router-dom";
 import Navbar from "../components/Narbar";
+import Footer from "../components/Footer";
 
 const HomeLayout = () =>{
     return(
@@ -8,6 +9,7 @@ const HomeLayout = () =>{
             <main className="flex-1"> 
                 <Outlet/> 
             </main>
+            <Footer/>
         </div>
     );
 };
