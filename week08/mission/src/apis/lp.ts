72013a78424afcc0ceeef22433ddfabb529e70e0
zo0o0 <hjoo1207@naver.com>
2025-05-21 04:23:54 +0900

    8주차 미션1 구현 완료

---
+++ week08/mission/src/apis/lp.ts
@@ -0,0 +1,49 @@
+import { PaginationDto } from "../types/common";
+import { ResponseLpListDto, UpdateLpsDto } from "../types/lp";
+import { axiosInstance } from "./axios";
+import { CreateLpsDto } from "../types/lp";
+
+export const getLpList = async ({cursor, limit, search, order,}: PaginationDto): Promise<ResponseLpListDto> => {
+  const { data } = await axiosInstance.get("/v1/lps", {
+    params: { cursor, limit, search, order },
+  });
+  
+  return data;
+};
+
+export const postLike = async (lpId: number) => {
+  const { data } = await axiosInstance.post(`/v1/lps/${lpId}/likes`);
+  return data;
+};
+
+export const postUnlike = async (lpId: number) => {
+  const { data } = await axiosInstance.delete(`/v1/lps/${lpId}/likes`);
+  return data;
+};
+
+export const createLp = async (data: CreateLpsDto) => {
+  const token = localStorage.getItem("accessToken");
+
+  return axiosInstance.post("/v1/lps", data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+};
+
+export const updateLp = async (lpId: number, data: UpdateLpsDto) => {
+  const token = localStorage.getItem("accessToken");
+
+  return axiosInstance.patch(`/v1/lps/${lpId}`, data, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+}
+
+export const deleteLp = async (lpId: number) => {
+  const { data } = await axiosInstance.delete(`/v1/lps/${lpId}`);
+  return data;
+}
---
+++ week08/mission/src/pages/LoginPage.tsx
@@ -0,0 +1,104 @@
+import { UserSigninInformation, validateSignin } from "../utils/validate";
+import useForm from "../hooks/useForm";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.tsx";
+import { useEffect } from "react";
+import { useMutation } from "@tanstack/react-query";
+
+const LoginPage = () => {
+  const navigate = useNavigate();
+  const { login, accessToken } = useAuth();
+
+  useEffect(() => {
+    if (accessToken) {
+      navigate("/");
+    }
+  }, [navigate, accessToken]);
+
+  const { values, errors, touched, getInputProps } = useForm<UserSigninInformation>({
+    initialValue: {
+      email: "",
+      password: "",
+    },
+    validate: validateSignin,
+  });
+
+  const mutation = useMutation({
+    mutationFn: login,
+    onSuccess: () => navigate("/"),
+    onError: (error) => {
+      alert("로그인 실패: 이메일 또는 비밀번호를 확인해주세요.");
+      console.error(error);
+    },
+  });
+
+  const handleSubmit = () => {
+    mutation.mutate(values);
+  };
+
+  const onGoogleLogin = () => {
+    window.location.href = import.meta.env.VITE_SERVER_API_URL + "/v1/auth/google/login";
+  };
+
+  const isDisabled =
+    Object.values(errors || {}).some((error) => error.length > 0) ||
+    Object.values(values).some((value) => value === "");
+
+  return (
+    <div className="flex flex-col items-center justify-center h-full gap-4">
+      <div className="flex flex-col gap-3">
+        <div className="flex items-center justify-between w-[300px] mb-2">
+          <button onClick={() => navigate(-1)} className="text-2xl">
+            &lt;
+          </button>
+          <h2 className="text-xl font-semibold text-center flex-1 mr-6">로그인</h2>
+        </div>
+
+        <button
+          onClick={onGoogleLogin}
+          className="flex items-center justify-center gap-2 border border-gray-400 rounded-md px-4 py-2 w-[300px] font-medium hover:bg-gray-100"
+        >
+          <img
+            src="https://www.svgrepo.com/show/475656/google-color.svg"
+            alt="Google"
+            className="w-5 h-5"
+          />
+          구글 로그인
+        </button>
+
+        <input
+          {...getInputProps("email")}
+          name="email"
+          className={`border border-[#ccc] w-[300px] p-[10px] focus.border-[#87bff] rounded-sm
+            ${errors?.email && touched?.email ? "border-red-500 bg-red-200" : "border-gray-300"}`}
+          type={"email"}
+          placeholder={"이메일"}
+        />
+        {errors?.email && touched?.email && (
+          <div className="text-red-500 text-sm">{errors.email}</div>
+        )}
+        <input
+          {...getInputProps("password")}
+          name="password"
+          className={`border border-[#ccc] w-[300px] p-[10px] focus.border-[#87bff] rounded-sm
+            ${errors?.password && touched?.password ? "border-red-500 bg-red-200" : "border-gray-300"}`}
+          type={"password"}
+          placeholder={"비밀번호"}
+        />
+        {errors?.password && touched?.password && (
+          <div className="text-red-500 text-sm">{errors.password}</div>
+        )}
+        <button
+          type="button"
+          onClick={handleSubmit}
+          disabled={isDisabled || mutation.isPending}
+          className="w-full bg-blue-600 text-white py-3 rounded-md text-lg font-medium hover:bg-blue-700 transition-colors cursor-pointer disabled:bg-gray-300"
+        >
+          {mutation.isPending ? "로그인 중..." : "로그인"}
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
