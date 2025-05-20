820f21949bcf7161d4a1f720d8214e199f8e8b63
zo0o0 <hjoo1207@naver.com>
2025-05-21 04:32:13 +0900

    검색 페이지 lp 사이즈 조정

--- week08/mission/src/pages/SearchPage.tsx
+++ week08/mission/src/pages/SearchPage.tsx
@@ -52,7 +52,7 @@ const SearchPage = () => {
       {isError && <p className="text-red-500">검색 중 오류 발생</p>}
       {results.length === 0 && debouncedInput && !isFetching && <p>검색 결과 없음</p>}
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-4">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4">
         {results.map((lp) => (
           <div
             onClick={() => navigate(`/lp/${lp.id}`)}
