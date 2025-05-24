effccb798c6bf6629184e910b5f7cfb9ceb50d22
onebone <jyc00410@gmail.com>
2025-05-25 04:13:39 +0900

    week09: init

---
+++ week09/src/App.tsx
@@ -0,0 +1,23 @@
+import {Navigation} from "./Navigation.tsx"
+import cartItems from "./assets/cartItems.ts";
+import {PlaylistItem} from "./PlaylistItem.tsx";
+
+function App() {
+    return (
+        <>
+            <Navigation/>
+
+            <div className="flex flex-col gap-4 mx-80 my-8">
+                {
+                    cartItems.map(item =>
+                        <PlaylistItem
+                            playlistItem={item}
+                            onAdd={() => {}}
+                            onRemove={() => {}} />)
+                }
+            </div>
+        </>
+    )
+}
+
+export default App
