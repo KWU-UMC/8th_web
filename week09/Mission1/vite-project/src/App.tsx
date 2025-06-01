a233bdb641a5313c271f10fd9f48e57e7bbe23d7
seoyeon0103 <angel6462@naver.com>
2025-06-01 18:51:16 +0900

    Feat: cartItems.ts

---
+++ week09/Mission1/vite-project/src/App.tsx
@@ -0,0 +1,8 @@
+import './App.css';
+
+function App() {
+
+  return <p className='text-blue-500'> BYE</p>;
+}
+
+export default App
---
+++ week09/Mission1/vite-project/src/constants/cartItems.ts
@@ -0,0 +1,124 @@
+const cartItems=[
+{
+        "id": 1,
+        "title": "Vespillo acsi despecto terebro vivo depromo tardus incidunt.",
+        "content": "Centum arma deripio solitudo. Comptus accusator calco tergo. Amplus celo ascit aggredior vester talus inventore.\nTenetur accusator ubi celebrer viridis tenetur admitto victoria validus sopor. Contabesco adnuo aeternus ratione cerno temporibus alioqui. Avarus umbra conculco caterva dolore supra acidus.\nSaepe crebro explicabo decimus. Aduro cernuus deprecator. Viscus ait sponte.",
+        "thumbnail": "https://loremflickr.com/2867/26?lock=6806845246526118",
+        "published": true,
+        "authorId": 1,
+        "createdAt": "2025-04-13T11:51:39.465Z",
+        "updatedAt": "2025-04-13T11:51:39.465Z",
+        "tags": [],
+        "likes": []
+    },
+    {
+        "id": 2,
+        "title": "Deleniti tego voluptas surculus auctus venio somnus cursim crinis.",
+        "content": "Commodi admiratio toties veritatis admiratio doloremque tabernus modi numquam. Cilicium stabilis aqua aro delectatio supellex animi voro. Arto tabernus aegrotatio adsum acceptus deduco velit.\nMaiores sodalitas civis sustineo carbo utroque canis sit derelinquo tunc. Uberrime arto eum sophismata patruus placeat architecto paens. Video textus sono.\nTunc comparo vestrum. Comptus cognomen conventus tunc acquiro aequitas stipes. Autem somniculosus quidem crur viriliter deripio soluta cavus colligo bellicus.",
+        "thumbnail": "https://loremflickr.com/3198/3499?lock=7135094028665001",
+        "published": true,
+        "authorId": 1,
+        "createdAt": "2025-04-13T11:51:39.478Z",
+        "updatedAt": "2025-04-13T11:51:39.478Z",
+        "tags": [],
+        "likes": []
+    },
+    {
+        "id": 3,
+        "title": "Defetiscor inflammatio cariosus molestiae vix tabernus aliquid deripio minus victus.",
+        "content": "Non careo cattus. Sublime avarus degero velociter cotidie clamo vita capillus reprehenderit aegrus. Trado delectatio suffragium alius optio cubitum accusantium cubicularis capto.\nCometes thymum callide coma. Valens curvo architecto thesis debeo conqueror earum cubitum cornu. Sursum deripio desolo tepesco aiunt sumptus.\nSuus magni apto somniculosus. Atrox veritas paens laborum acquiro amor trucido ara spargo. Thesaurus tondeo sulum.",
+        "thumbnail": "https://loremflickr.com/2649/1362?lock=2851096712952609",
+        "published": true,
+        "authorId": 7,
+        "createdAt": "2025-04-13T11:51:39.482Z",
+        "updatedAt": "2025-04-13T11:51:39.482Z",
+        "tags": [],
+        "likes": []
+    },
+    {
+        "id": 4,
+        "title": "Defluo tenuis dens soluta adflicto teres beatae confero.",
+        "content": "Vorax talis amor dedecor patrocinor venia deripio. Aro ager vitium. Vilis aveho vulticulus antepono cognatus.\nTemeritas agnosco acervus delectatio totidem videlicet inventore impedit. Comburo vos vereor adsidue doloremque coma. Voluptatum quasi aliquid.\nSuper cibo appono decumbo dapifer. Cito cras sustineo ante peccatus vae usus certus considero constans. Degusto antiquus cohibeo possimus vestrum alveus quos sodalitas laborum conor.",
+        "thumbnail": "https://loremflickr.com/1328/1327?lock=2416444989259038",
+        "published": true,
+        "authorId": 1,
+        "createdAt": "2025-04-13T11:51:39.488Z",
+        "updatedAt": "2025-04-13T11:51:39.488Z",
+        "tags": [],
+        "likes": []
+    },
+    {
+        "id": 5,
+        "title": "Dolore cicuta clarus utrimque cuius adeptio.",
+        "content": "Viscus ultra barba super. Adipisci amita degenero correptius basium. Templum aro avarus aqua calamitas delectatio audio aut verto adiuvo.\nVideo accusamus considero capitulus. Ipsa theca clementia explicabo peccatus maxime color pauci ventosus delicate. Versus adiuvo recusandae hic deserunt coerceo umbra asperiores volva adicio.\nAttonbitus ipsa vado averto. Deserunt tepesco amplexus ventito allatus. Quam ex sint beneficium sumo adeo.",
+        "thumbnail": "https://loremflickr.com/649/1897?lock=6550398601197276",
+        "published": true,
+        "authorId": 1,
+        "createdAt": "2025-04-13T11:51:39.493Z",
+        "updatedAt": "2025-04-13T11:51:39.493Z",
+        "tags": [],
+        "likes": []
+    },
+    {
+        "id": 6,
+        "title": "Custodia amet vacuus sto atrox quis.",
+        "content": "Spiculum depromo deorsum cuppedia concedo solutio. Ante commodo ater desidero chirographum thermae contabesco charisma in. Recusandae trans utilis desino cubicularis calculus.\nVobis combibo claustrum. Vaco odio trucido advoco centum abscido illum. Copiose sonitus studio constans.\nConcedo credo officia adulescens undique paulatim. Tempore agnosco numquam vulgivagus. Iste attollo sollicito depereo catena territo demo callide.",
+        "thumbnail": "https://loremflickr.com/3601/3086?lock=2622541820092371",
+        "published": true,
+        "authorId": 6,
+        "createdAt": "2025-04-13T11:51:39.498Z",
+        "updatedAt": "2025-04-13T11:51:39.498Z",
+        "tags": [],
+        "likes": []
+    },
+    {
+        "id": 7,
+        "title": "Combibo teres nostrum certe suggero uxor cibus atrox cursus vivo.",
+        "content": "Thesis confugo tardus. Depopulo audacia vobis traho. Denique thema villa tardus suus speciosus vulticulus universe.\nUstulo spes benigne rem vivo summisse volva ubi delibero. Depraedor sollers vita statua aranea laboriosam. Pecto confero occaecati aestas rem denuo.\nTumultus laboriosam adfero. Comitatus aufero tunc inventore ipsa aro vulticulus. Quod colligo strues.",
+        "thumbnail": "https://loremflickr.com/575/2691?lock=3175117186231716",
+        "published": true,
+        "authorId": 4,
+        "createdAt": "2025-04-13T11:51:39.504Z",
+        "updatedAt": "2025-04-13T11:51:39.504Z",
+        "tags": [],
+        "likes": []
+    },
+    {
+        "id": 8,
+        "title": "Territo caveo tracto.",
+        "content": "Architecto vigilo aveho celer. Comptus urbs compono distinctio arx architecto quos. Adicio absorbeo aduro theologus stabilis bibo inflammatio solio unde tunc.\nEius tredecim consectetur volup curvo vorax agnitio taedium tactus defessus. Concedo beneficium in. Autus dignissimos virga aetas adeo.\nVel ascit sumo vergo celo blandior quibusdam charisma explicabo. Corroboro voveo consectetur. Vulnus animadverto tardus volubilis cur.",
+        "thumbnail": "https://loremflickr.com/1133/3265?lock=2120575098445249",
+        "published": true,
+        "authorId": 7,
+        "createdAt": "2025-04-13T11:51:39.510Z",
+        "updatedAt": "2025-04-13T11:51:39.510Z",
+        "tags": [],
+        "likes": []
+    },
+    {
+        "id": 9,
+        "title": "Testimonium conduco victoria vomito celebrer cubo uredo vulariter.",
+        "content": "Venia cimentarius laboriosam decor creo patior facilis. Templum patrocinor vicinus. Patrocinor antepono quia tero ad tristis arca pauper crudelis dedecor.\nCarus cubicularis victus totus patria bellum ultio iusto. Vestigium tibi magni textilis aspernatur aranea antiquus taceo patior. Sto tripudio aliquam varietas consequuntur.\nAdemptio calculus templum video concido. Cattus non eius tendo ustulo deporto cur et. Benigne sustineo aiunt veniam vita vilis atque.",
+        "thumbnail": "https://loremflickr.com/1919/1193?lock=1606453882735884",
+        "published": true,
+        "authorId": 7,
+        "createdAt": "2025-04-13T11:51:39.516Z",
+        "updatedAt": "2025-04-13T11:51:39.516Z",
+        "tags": [],
+        "likes": []
+    },
+    {
+        "id": 10,
+        "title": "Sub tumultus adhaero cohaero ceno.",
+        "content": "Minima amor tredecim degusto pectus stips vulgus. Dedecor arcesso assumenda demens colligo undique victus succurro casus consuasor. Viscus compono defungo sto valens.\nSed aro ubi utor voluptas cariosus uter speciosus corroboro tunc. Dedico cursim tepesco bestia aetas vesper confero. Corpus cupiditate modi artificiose pecus accedo.\nTamquam angustus testimonium tot credo. Attollo cimentarius adfectus. Viriliter adipiscor ustulo celebrer aduro tepesco comparo synagoga.",
+        "thumbnail": "https://loremflickr.com/212/1765?lock=7987874148693015",
+        "published": true,
+        "authorId": 2,
+        "createdAt": "2025-04-13T11:51:39.523Z",
+        "updatedAt": "2025-04-13T11:51:39.523Z",
+        "tags": [],
+        "likes": []
+    }
+];
+
+export default cartItems;
